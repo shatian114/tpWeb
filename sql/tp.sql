ed3a0f99df308959d7170cd1d76f7eab34c7eaeb
peng <1320024819@qq.com>
2016-03-18 22:15:47 +0800

    修复了关注方面的bug，新增了签到功能

--- sql/tp.sql
+++ sql/tp.sql
@@ -1,10 +1,22 @@
+#用户的信息表
 create table user (
-	id bigint auto_increment primary key,
+	id int unsigned auto_increment primary key,
 	name varchar(30),
 	password char(20)
 );
 
+#图片信息的表
 create table imgInfo (
 	url varchar(200),
-	propritary bigint
+	propritary int
+);
+
+#用户直接相互关系的表，如关注，粉丝
+create table userRelation (
+	#粉丝的id
+	fId int unsigned,
+	#关注者的id
+	gId int unsigned,
+	#关系,0为关注，为好友
+	relation char(1)
 );
--- userManager/userManager.php
+++ userManager/userManager.php
@@ -16,14 +16,15 @@ switch($_POST['type']){
 			$result = $db->query('select id from user where name="'.$_POST['name'].'"');
 			$result = $result->fetch_assoc();
 			$uid = $result['id'];
-			$db->query('create table gx'.$uid.'(uid bigint, gx char(1))');
+			//创建签到表
+			$db->query('create table checkIn'.$uid.'(checkInDate date, checkInTime time, continuousNum int unsigned)');
 			echo '注册成功!';
 		}
 		break;
 	//登陆
 	case 'login':
 		//查找用户名是否存在，存在再对比密码
-		$result = $db->query('select * from user where name="'.$_POST['Name'].'"');
+		$result = $db->query('select * from user where name="'.$_POST['name'].'"');
 		if($result->num_rows > 0){
 			$password = $result->fetch_assoc()['password'];
 			if($password === $_POST['password']){
@@ -39,6 +40,10 @@ switch($_POST['type']){
 	case 'logout':
 		unset($_SESSION['name']);
 		echo 'logoutOk';
+		break;
+	case 'viewLogin':
+		echo $_SESSION['name'];
+		break;
 	case 'attention':
 		//查找被关注的用户的id
 		$result = $db->query('select id from user where name="'.$_POST['name'].'"')->fetch_assoc();
@@ -46,20 +51,60 @@ switch($_POST['type']){
 		//查找本用户的id
 		$result = $db->query('select id from user where name="'.$_SESSION['name'].'"')->fetch_assoc();
 		$id = $result['id'];
-		//在被关注的用户的关系表里查找是否有本用户，有的话则设置互为好友
-		//h:好友，f:粉丝，g:关注的人
-		$result = $db->query('select * from gx'.$bId.' where uid="'.$id.'"');
-		if($result->rows_num > 0){
-			$db->query('insert into gx'.$id.' (uid, gx) values('.$bId.', "h")');
-			$db->query('insert into gx'.$bId.' (uid, gx) values('.$id.', "h")');
-			//关注成功，已互为好友
-			echo 'hyOk';
+		//查看是否为粉丝，是的话设置为好友，不是则设置为粉丝
+		$result = $db->query('select * from userRelation where fId="'.$bId.'" and gId="'.$id.'"');
+		if($result->num_rows > 0){
+			echo 'is hy';
+			$db->query('update userRelation set relation="1" where fId="'.$bId.'" and gId="'.$id.'"');
 		}else{
-			$db->query('insert into gx'.$id.' (uid, gx) values('.$bId.', "g")');
-			$db->query('insert into gx'.$bId.' (uid, gx) values('.$id.', "f")');
-			//关注成功，已成为其粉丝
-			echo 'gzOk';
+			echo 'not is hy';
+			$db->query('insert into userRelation (fId, gId, relation) values('.$id.', '.$bId.', "0")');
 		}
+		break;
+	case 'noAttention':
+		//查找被关注的用户的id
+		$result = $db->query('select id from user where name="'.$_POST['name'].'"')->fetch_assoc();
+		$bId = $result['id'];
+		//查找本用户的id
+		$result = $db->query('select id from user where name="'.$_SESSION['name'].'"')->fetch_assoc();
+		$id = $result['id'];
+		//如果不是好友，只是粉丝，则删除记录
+		$result = $db->query('select * from userRelation where fId='.$id.' and gId='.$bId.' and relation="0"');
+		if($result->num_rows > 0){
+			echo 'not is hy';
+			$db->query('delete from userRelation where fId='.$id.' and gId='.$bId);
+			break;
+		}
+		//如果是好友，则设置为关注
+		$result = $db->query('select * from userRelation where fId='.$id.' and gId='.$bId);
+		if($result->num_rows > 0){
+			echo 'is hy1';
+			$db->query('delete from userRelation where fId='.$id.' and gId='.$bId);
+		}
+		$result = $db->query('select * from userRelation where fId='.$bId.' and gId='.$id);
+		if($result->num_rows > 0){
+			echo 'is hy';
+			$db->query('delete from userRelation where fId='.$bId.' and gId='.$id);
+		}
+		$db->query('insert into userRelation (fId, gId, relation) values('.$bId.', '.$id.', "0")');
+		break;
+	case 'checkIn':
+		//查找当前用户的id
+		$result = $db->query('select * from user where name="'.$_SESSION['name'].'"')->fetch_assoc();
+		$uId = $result['id'];
+		//查找昨天是否签到
+		$continuousNum = 1;
+		$result = $db->query('select continuousNum from checkIn'.$uId.' where CheckInDate=date_add(curdate(), interval -1 day)');
+		if($result){
+			echo 'continuousNum';
+			$result = $result->fetch_assoc();
+			$continuousNum = $result['continuousNum'] + 1;
+		}
+		//如果今天的签到表不存在，则创建
+		$db->query('create table if not exists checkIn_'.date("Ymd").'(id int unsigned primary key auto_increment, continuousNum int unsigned, uId int unsigned, checkInDate date, checkInTime time)');
+		//在今天的签到表里插入签到数据
+		$db->query('insert into checkIn_'.date("Ymd").' (uId, continuousNum, checkInDate, checkInTime) values('.$uId.','.$continuousNum.', curdate(), curtime())');
+		$db->query('insert into checkIn'.$uId.' (checkInDate, checkInTime, continuousNum) values(curdate(), curtime(), '.$continuousNum.')');
 		break;
 	default:
 		break;
