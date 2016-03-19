b81f20323a56552b88cea5df054d5795a50cbcb9
shatian114 <1320024819@qq.com>
2016-03-19 17:48:49 +0800

    优化了用户id的查询，新增了用户粉丝、关注、好友的列表的显示。关注、签到基本功能目前已完成

--- userManager/userManager.php
+++ userManager/userManager.php
@@ -26,9 +26,11 @@ switch($_POST['type']){
 		//查找用户名是否存在，存在再对比密码
 		$result = $db->query('select * from user where name="'.$_POST['name'].'"');
 		if($result->num_rows > 0){
-			$password = $result->fetch_assoc()['password'];
+			$result = $result->fetch_assoc();
+			$password = $result['password'];
 			if($password === $_POST['password']){
 				$_SESSION['name'] = $_POST['name'];
+				$_SESSION['uid'] = $result['id'];
 				echo 'ok';
 			}else{
 				echo 'passwordError';
@@ -48,53 +50,44 @@ switch($_POST['type']){
 		//查找被关注的用户的id
 		$result = $db->query('select id from user where name="'.$_POST['name'].'"')->fetch_assoc();
 		$bId = $result['id'];
-		//查找本用户的id
-		$result = $db->query('select id from user where name="'.$_SESSION['name'].'"')->fetch_assoc();
-		$id = $result['id'];
 		//查看是否为粉丝，是的话设置为好友，不是则设置为粉丝
-		$result = $db->query('select * from userRelation where fId="'.$bId.'" and gId="'.$id.'"');
+		$result = $db->query('select * from userRelation where fId="'.$bId.'" and gId="'.$_SESSION['uid'].'"');
 		if($result->num_rows > 0){
 			echo 'is hy';
-			$db->query('update userRelation set relation="1" where fId="'.$bId.'" and gId="'.$id.'"');
+			$db->query('update userRelation set relation="1" where fId="'.$bId.'" and gId="'.$_SESSION['uid'].'"');
 		}else{
 			echo 'not is hy';
-			$db->query('insert into userRelation (fId, gId, relation) values('.$id.', '.$bId.', "0")');
+			$db->query('insert into userRelation (fId, gId, relation) values('.$_SESSION['uid'].', '.$bId.', "0")');
 		}
 		break;
 	case 'noAttention':
 		//查找被关注的用户的id
 		$result = $db->query('select id from user where name="'.$_POST['name'].'"')->fetch_assoc();
 		$bId = $result['id'];
-		//查找本用户的id
-		$result = $db->query('select id from user where name="'.$_SESSION['name'].'"')->fetch_assoc();
-		$id = $result['id'];
 		//如果不是好友，只是粉丝，则删除记录
-		$result = $db->query('select * from userRelation where fId='.$id.' and gId='.$bId.' and relation="0"');
+		$result = $db->query('select * from userRelation where fId='.$_SESSION['uid'].' and gId='.$bId.' and relation="0"');
 		if($result->num_rows > 0){
 			echo 'not is hy';
-			$db->query('delete from userRelation where fId='.$id.' and gId='.$bId);
+			$db->query('delete from userRelation where fId='.$_SESSION['uid'].' and gId='.$bId);
 			break;
 		}
 		//如果是好友，则设置为关注
-		$result = $db->query('select * from userRelation where fId='.$id.' and gId='.$bId);
+		$result = $db->query('select * from userRelation where fId='.$_SESSION['uid'].' and gId='.$bId);
 		if($result->num_rows > 0){
 			echo 'is hy1';
-			$db->query('delete from userRelation where fId='.$id.' and gId='.$bId);
+			$db->query('delete from userRelation where fId='.$_SESSION['uid'].' and gId='.$bId);
 		}
-		$result = $db->query('select * from userRelation where fId='.$bId.' and gId='.$id);
+		$result = $db->query('select * from userRelation where fId='.$bId.' and gId='.$_SESSION['uid']);
 		if($result->num_rows > 0){
 			echo 'is hy';
-			$db->query('delete from userRelation where fId='.$bId.' and gId='.$id);
+			$db->query('delete from userRelation where fId='.$bId.' and gId='.$_SESSION['uid']);
 		}
-		$db->query('insert into userRelation (fId, gId, relation) values('.$bId.', '.$id.', "0")');
+		$db->query('insert into userRelation (fId, gId, relation) values('.$bId.', '.$_SESSION['uid'].', "0")');
 		break;
 	case 'checkIn':
-		//查找当前用户的id
-		$result = $db->query('select * from user where name="'.$_SESSION['name'].'"')->fetch_assoc();
-		$uId = $result['id'];
 		//查找昨天是否签到
 		$continuousNum = 1;
-		$result = $db->query('select continuousNum from checkIn'.$uId.' where CheckInDate=date_add(curdate(), interval -1 day)');
+		$result = $db->query('select continuousNum from checkIn'.$_SESSION['uid'].' where CheckInDate=date_add(curdate(), interval -1 day)');
 		if($result){
 			echo 'continuousNum';
 			$result = $result->fetch_assoc();
@@ -103,8 +96,52 @@ switch($_POST['type']){
 		//如果今天的签到表不存在，则创建
 		$db->query('create table if not exists checkIn_'.date("Ymd").'(id int unsigned primary key auto_increment, continuousNum int unsigned, uId int unsigned, checkInDate date, checkInTime time)');
 		//在今天的签到表里插入签到数据
-		$db->query('insert into checkIn_'.date("Ymd").' (uId, continuousNum, checkInDate, checkInTime) values('.$uId.','.$continuousNum.', curdate(), curtime())');
-		$db->query('insert into checkIn'.$uId.' (checkInDate, checkInTime, continuousNum) values(curdate(), curtime(), '.$continuousNum.')');
+		$db->query('insert into checkIn_'.date("Ymd").' (uId, continuousNum, checkInDate, checkInTime) values('.$_SESSION['uid'].','.$continuousNum.', curdate(), curtime())');
+		$db->query('insert into checkIn'.$_SESSION['uid'].' (checkInDate, checkInTime, continuousNum) values(curdate(), curtime(), '.$continuousNum.')');
+		break;
+	case 'friend':
+		$friendList = array();
+		//查找在粉丝位置的好友
+		$result = $db->query('select fId from userRelation where relation="1" and gId='.$_SESSION['uid']);
+		$fNum = $result->num_rows;
+		for($i=0; $i<$fNum; $i++){
+			$row = $result->fetch_assoc();
+			$nameCol = $db->query('select name from user where id='.$row['fId'])->fetch_assoc();
+			$friendList[$i] = $nameCol['name'];
+		}
+		//查找在关注位置的好友
+		$result = $db->query('select gId from userRelation where relation="1" and fId='.$_SESSION['uid']);
+		$gNum = $result->num_rows;
+		for($i=0; $i<$gNum; $i++){
+			$row = $result->fetch_assoc();
+			$nameCol = $db->query('select name from user where id='.$row['gId'])->fetch_assoc();
+			$friendList[$i+$fNum] = $nameCol['name'];
+		}
+		echo json_encode(array('friendNum'=>($fNum+$gNum), 'friendList'=>$friendList));
+		break;
+	case 'fans':
+		$fansList = array();
+		//查找在粉丝位置的id
+		$result = $db->query('select fId from userRelation where relation="0" and gId='.$_SESSION['uid']);
+		$fansNum = $result->num_rows;
+		for($i=0; $i<$fansNum; $i++){
+			$row = $result->fetch_assoc();
+			$nameCol = $db->query('select name from user where id='.$row['fId'])->fetch_assoc();
+			$fansList[$i] = $nameCol['name'];
+		}
+		echo json_encode(array('fansNum'=>$fansNum, 'fansList'=>$fansList));
+		break;
+	case 'gz':
+		$gzList = array();
+		//查找在关注位置的id
+		$result = $db->query('select gId from userRelation where relation="0" and fId='.$_SESSION['uid']);
+		$gzNum = $result->num_rows;
+		for($i=0; $i<$gzNum; $i++){
+			$row = $result->fetch_assoc();
+			$nameCol = $db->query('select name from user where id='.$row['gId'])->fetch_assoc();
+			$gzList[$i] = $nameCol['name'];
+		}
+		echo json_encode(array('gzNum'=>$gzNum, 'gzList'=>$gzList));
 		break;
 	default:
 		break;
